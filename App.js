5ef2b3c234adff888c84fb448969e0a1996ba653
renadr <ladrien2@gmail.com>
2018-03-25 22:11:25 +0200

    Create two view with a Horizontal Scroll

--- App.js
+++ App.js
@@ -1,14 +1,25 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Dimensions, ScrollView, StatusBar, Image, TextInput } from 'react-native';
 
 export default class App extends React.Component {
   render() {
+    let screenWidth = Dimensions.get('window').width;
+    let screenHeight = Dimensions.get('window').height;
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
+      <ScrollView horizontal={true}>
+        <View style={{backgroundColor:'#5f9eff',flex:1,width:screenWidth,justifyContent:'center',alignItems:'center'}}>
+          <StatusBar hidden />
+          <Text style={{fontSize:20,padding:15,color:'white',textAlign:'center'}}>
+            Ecran 1
+          </Text>
+        </View>
+        <View style={{backgroundColor:'#5f9e02',flex:1,width:screenWidth,justifyContent:'center',alignItems:'center'}}>
+          <StatusBar hidden />
+          <Text style={{fontSize:20,padding:15,color:'white',textAlign:'center'}}>
+            Ecran 2
+          </Text>
+        </View>
+      </ScrollView>
     );
   }
 }
