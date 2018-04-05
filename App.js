1832ce381e0caf07de0f5724279a78fa581128ef
GautierL4 <gautier.laurent4@gmail.com>
2018-04-05 09:52:05 +0200

    Ajout du fichier component

--- App.js
+++ App.js
@@ -1,8 +1,66 @@
 import React from 'react';
-import { StyleSheet, Text, View, Dimensions, ScrollView, StatusBar, Image, TextInput } from 'react-native';
+import { StyleSheet, Text, View, Dimensions, ScrollView, StatusBar, Image, TextInput, ActivityIndicator } from 'react-native';
+import { key } from './key.js';
 
 export default class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state={
+      day: '',
+      type: '',
+      tempText: '',
+      tempAvg: '',
+      isLoading: true
+    }
+  }
+
+  componentDidMount(){
+    this.setState({ isLoading: true })
+
+    const APIBaseURL = "http://api.wunderground.com/api/";
+    const APIService = "/forecast/q/";
+    const API = APIBaseURL + key + APIService ;
+    const headers = {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    };
+    const country = "FR";
+    const city = "Paris";
+    const specificData = country+"/"+city+".json";
+    // Getting Data from API
+    return fetch(API + specificData)
+    .then((response) => response.json())
+    .then((responseJson) => {
+      if(responseJson.response.features.forecast == 1){
+        // Updating state with Data
+        this.setState(
+          {
+            isLoading: false,
+            day: responseJson.forecast.simpleforecast.forecastday[0].date.weekday,
+            type: responseJson.forecast.simpleforecast.forecastday[0].conditions,
+            tempText: responseJson.forecast.simpleforecast.forecastday[0].low.celsius + ' °C / ' + responseJson.forecast.simpleforecast.forecastday[0].high.celsius + ' °C',
+            tempAvg: (parseInt(responseJson.forecast.simpleforecast.forecastday[0].low.celsius) + parseInt(responseJson.forecast.simpleforecast.forecastday[0].high.celsius)) / 2
+          })
+      }
+      else{
+        alert("Il y a eu une erreur lors de la requete (Status not OK)");
+      }
+    }).catch((error) => {
+      console.error(error);
+    });
+  }
+
+  // If state is loading, display a spinner
   render() {
+    if(this.state.isLoading){
+      return(
+        <View style={{flex: 1, padding: 20}}>
+          <ActivityIndicator/>
+        </View>
+      )
+    }
+
+
     let screenWidth = Dimensions.get('window').width;
     let screenHeight = Dimensions.get('window').height;
     let pic = {
@@ -17,7 +75,7 @@ export default class App extends React.Component {
           <StatusBar hidden />
           <View style={{flex:5}}>
             <View style={{flexDirection:'row',flex:1,alignItems: 'center',justifyContent: 'center'}}>
-              <Text style={{color:'#ffffff',fontSize:25}}>Aujourd'hui</Text>
+              <Text style={{color:'#ffffff',fontSize:25}}>{this.state.day}</Text>
             </View>
             <View style={{flexDirection:'row',flex:1,alignItems: 'center',justifyContent: 'center'}}>
               <Text style={{color:'#ffffff',fontSize:15}}>Paris, France</Text>
@@ -28,13 +86,13 @@ export default class App extends React.Component {
               </View>
             </View>
             <View style={{flexDirection:'row',flex:1,alignItems: 'center',justifyContent: 'center'}}>
-              <Text style={{color:'#ffffff',fontSize:20}}>Ensoleillé</Text>
+              <Text style={{color:'#ffffff',fontSize:20}}>{this.state.type}</Text>
             </View>
             <View style={{flexDirection:'row',flex:2,alignItems: 'center',justifyContent: 'center'}}>
-              <Text style={{color:'#ffffff',fontSize:90,textShadowColor: 'rgba(0, 0, 0, 0.1)', textShadowOffset: {width: 0, height: 1}, textShadowRadius: 30, }}>13°C</Text>
+              <Text style={{color:'#ffffff',fontSize:90,textShadowColor: 'rgba(0, 0, 0, 0.1)', textShadowOffset: {width: 0, height: 1}, textShadowRadius: 30, }}>{this.state.tempAvg+" °C"}</Text>
             </View>
             <View style={{flexDirection:'row',flex:1,alignItems: 'center',justifyContent: 'center'}}>
-              <Text style={{color:'#ffffff',fontSize:20}}>8°C / 13°C</Text>
+              <Text style={{color:'#ffffff',fontSize:20}}>{this.state.tempText}</Text>
             </View>
           </View>
           <View style={{flex:1}}>
