257880f4014bc538b53d94369afb90638a7b2914
GautierL4 <gautier.laurent4@gmail.com>
2018-04-05 22:55:49 +0200

    Separate API Logic from main files

--- App.js
+++ App.js
@@ -1,53 +1,32 @@
 import React from 'react';
 import { StyleSheet, Text, View, Dimensions, ScrollView, StatusBar, Image, TextInput, ActivityIndicator } from 'react-native';
-import { key } from './key.js';
+import { key } from './key';
 import { Screen } from './components/Screen';
+import APIHandler from './components/API/APIHandler';
+
+const APIManager = new APIHandler();
 
 export default class App extends React.Component {
   constructor(props){
     super(props);
     this.state={
-      day: '',
-      type: '',
-      tempText: '',
-      tempAvg: '',
-      isLoading: true
-    }
+      isLoading: true,
+      data: null
+    };
   }
 
   componentDidMount(){
+    // Setting loading status
     this.setState({ isLoading: true })
-
-    const APIBaseURL = "http://api.wunderground.com/api/";
-    const APIService = "/forecast/q/";
-    const API = APIBaseURL + key + APIService ;
-    const headers = {
-      "Content-Type": "application/json",
-      Accept: "application/json",
-    };
-    const country = "FR";
-    const city = "Paris";
-    const specificData = country+"/"+city+".json";
-    // Getting Data from API
-    return fetch(API + specificData)
-    .then((response) => response.json())
-    .then((responseJson) => {
-      if(responseJson.response.features.forecast == 1){
-        // Updating state with Data
-        this.setState(
-          {
-            isLoading: false,
-            day: responseJson.forecast.simpleforecast.forecastday[0].date.weekday,
-            type: responseJson.forecast.simpleforecast.forecastday[0].conditions,
-            tempText: responseJson.forecast.simpleforecast.forecastday[0].low.celsius + ' °C / ' + responseJson.forecast.simpleforecast.forecastday[0].high.celsius + ' °C',
-            tempAvg: (parseInt(responseJson.forecast.simpleforecast.forecastday[0].low.celsius) + parseInt(responseJson.forecast.simpleforecast.forecastday[0].high.celsius)) / 2
-          })
-      }
-      else{
-        alert("Il y a eu une erreur lors de la requete (Status not OK)");
-      }
-    }).catch((error) => {
-      console.error(error);
+    // Using Promise, getting API data then setting the state
+    APIManager.getDataFromAPI().then(data => {
+      console.log(data);
+      this.setState(
+        {
+          isLoading: false,
+          data: data,
+        }
+      )
     });
   }
 
@@ -55,19 +34,32 @@ export default class App extends React.Component {
   render() {
     if(this.state.isLoading){
       return(
-        <View style={{flex: 1, padding: 20}}>
+       <View style={{flex: 1, padding: 20}}>
           <ActivityIndicator/>
         </View>
       )
     }
 
-
-
     return (
       <ScrollView horizontal={true} pagingEnabled={true}>
-        <Screen day={this.state.day} type={this.state.type} tempText={this.state.tempText} tempAvg={this.state.tempAvg} />
-        <Screen day={this.state.day} type={this.state.type} tempText={this.state.tempText} tempAvg={this.state.tempAvg} />
-        <Screen day={this.state.day} type={this.state.type} tempText={this.state.tempText} tempAvg={this.state.tempAvg} />
+        <Screen
+          day={this.state.data.today.day}
+          type={this.state.data.today.type}
+          tempText={this.state.data.today.tempText}
+          tempAvg={this.state.data.today.tempAvg}
+        />
+        <Screen
+          day={this.state.data.tommorow.day}
+          type={this.state.data.tommorow.type}
+          tempText={this.state.data.tommorow.tempText}
+          tempAvg={this.state.data.tommorow.tempAvg}
+        />
+        <Screen
+          day={this.state.data.thirdDay.day}
+          type={this.state.data.thirdDay.type}
+          tempText={this.state.data.thirdDay.tempText}
+          tempAvg={this.state.data.thirdDay.tempAvg}
+        />
       </ScrollView>
     );
   }
---
+++ components/API/APIHandler.js
@@ -0,0 +1,78 @@
+import React from 'react';
+import { StyleSheet, Text, View, Dimensions, ScrollView, StatusBar, Image, TextInput, ActivityIndicator } from 'react-native';
+import { key } from '../../key.js';
+
+const APIBaseURL = "http://api.wunderground.com/api/";
+const APIService = "/forecast/q/";
+const API = APIBaseURL + key + APIService ;
+const headers = {
+    "Content-Type": "application/json",
+    Accept: "application/json",
+};
+
+class APIHandler {
+
+    async getDataFromAPI(){
+        try{
+            APIResponse = await this.getAPIResponseFromLocation();
+            weatherInfo = this.getWeatherInfoFromAPIResponse(APIResponse);
+            dayInfo = this.getDayInfoFromWeatherInfo(weatherInfo);
+            console.log(dayInfo);
+        } catch(e){
+            console.error(e);
+        }
+        return {
+            today: dayInfo[0],
+            tommorow: dayInfo[1],
+            thirdDay: dayInfo[2]
+        };
+    }
+
+
+    async getAPIResponseFromLocation(){
+        var country = "FR";
+        var city = "Paris";
+        var specificData = country+"/"+city+".json";  
+        try{
+            let response = await fetch(
+                API + specificData
+            );
+            responseJson =  await response.json();
+        }   catch (error){
+            console.error(error);
+        }
+        return responseJson;
+    }
+
+    getWeatherInfoFromAPIResponse(APIResponse){
+        days = [];
+        for(let i=0;i<3;i++){
+            days[i] = APIResponse.forecast.simpleforecast.forecastday[i];
+        }
+        return days;
+    }
+
+    getDayInfoFromWeatherInfo(weatherInfo){
+        dayInfo = [];
+        for(let i=0;i<weatherInfo.length;i++){
+            switch(i){
+                case 0:
+                    weatherInfo[i].date.weekday = "Today";
+                    break;
+                case 1:
+                    weatherInfo[i].date.weekday = "Tommorow";
+                    break;
+            }
+            dayInfo[i] = {
+                day: weatherInfo[i].date.weekday,
+                type: weatherInfo[i].conditions,
+                tempText : weatherInfo[i].low.celsius + ' °C / ' + weatherInfo[i].high.celsius + ' °C',
+                tempAvg : (parseInt(weatherInfo[i].low.celsius) + parseInt(weatherInfo[i].high.celsius)) / 2
+            };
+        }
+        return dayInfo;
+    }
+
+}
+
+module.exports = APIHandler;
