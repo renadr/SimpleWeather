d635b88c41c52651c2eff839a52015197781d63e
renadr <ladrien2@gmail.com>
2018-04-06 01:47:56 +0200

    Change the global style to something more modern and visible

--- components/Screen.js
+++ components/Screen.js
@@ -6,34 +6,35 @@ export class Screen extends React.Component {
         let screenWidth = Dimensions.get('window').width;
         let screenHeight = Dimensions.get('window').height;
         let pic = {
-          uri: 'http://adrienlemaire.fr/SimpleWeather/soleil.gif'
+          uri: '../icons/color/001-palm-tree.png'
         };
         // The background color change the night (8:00pm / 20h) and become darker 
         let d = new Date();
-        let colorBg = d.getHours()<20 ? '#FFDF00' : '#250051';
+        let colorBg = d.getHours()<20 && d.getHours()>6 ? '#FFFFFF' : '#333333';
+        let colorTxt = d.getHours()<20 && d.getHours()>6 ? '#333333' : '#FFFFFF';
         return (
             <View style={{backgroundColor:colorBg,flex:1,width:screenWidth,}}>
             <StatusBar hidden />
             <View style={{flex:5}}>
               <View style={{flexDirection:'row',flex:1,alignItems: 'center',justifyContent: 'center'}}>
-                <Text style={{color:'#ffffff',fontSize:25}}>{this.props.day}</Text>
+                <Text style={{color:colorTxt,fontSize:25,fontWeight: 'bold'}}>{this.props.day}</Text>
               </View>
               <View style={{flexDirection:'row',flex:1,alignItems: 'center',justifyContent: 'center'}}>
-                <Text style={{color:'#ffffff',fontSize:15}}>Paris, France</Text>
+                <Text style={{color:colorTxt,fontSize:15}}>Paris, France</Text>
               </View>
               <View style={{flexDirection:'row',flex:4, alignItems: 'center',justifyContent: 'center'}}>
-                <View style={{flex:1,flexDirection:'column',alignItems: 'stretch',justifyContent: 'center'}}>
-                  <Image source={pic} style={{flex:1}} resizeMode="contain" />
+                <View style={{flex:1,flexDirection:'column',alignItems: 'center',justifyContent: 'center'}}>
+                  <Image source={require('../icons/color/050-cloud.png')} style={{flex:1}} resizeMode="contain" />
                 </View>
               </View>
               <View style={{flexDirection:'row',flex:1,alignItems: 'center',justifyContent: 'center'}}>
-                <Text style={{color:'#ffffff',fontSize:20}}>{this.props.type}</Text>
+                <Text style={{color:colorTxt,fontSize:20}}>{this.props.type}</Text>
               </View>
               <View style={{flexDirection:'row',flex:2,alignItems: 'center',justifyContent: 'center'}}>
-                <Text style={{color:'#ffffff',fontSize:90,textShadowColor: 'rgba(0, 0, 0, 0.05)', textShadowOffset: {width: 0, height: 1}, textShadowRadius: 30, fontWeight: 'bold', }}>{this.props.tempAvg+" °C"}</Text>
+                <Text style={{color:colorTxt,fontSize:90,textShadowColor: 'rgba(0, 0, 0, 0.05)', textShadowOffset: {width: 0, height: 1}, textShadowRadius: 30, fontWeight: 'bold', }}>{this.props.tempAvg+" °C"}</Text>
               </View>
               <View style={{flexDirection:'row',flex:1,alignItems: 'center',justifyContent: 'center'}}>
-                <Text style={{color:'#ffffff',fontSize:20}}>{this.props.tempText}</Text>
+                <Text style={{color:colorTxt,fontSize:20}}>{this.props.tempText}</Text>
               </View>
             </View>
             <View style={{flex:1}}>
