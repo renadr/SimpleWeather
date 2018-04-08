7c2f4971cec7a30f8935a986698d0dbaeb10cbbd
renadr <ladrien2@gmail.com>
2018-04-08 19:40:50 +0200

    Finish the style of the search bar

--- components/SearchScreen.js
+++ components/SearchScreen.js
@@ -5,15 +5,14 @@ class SearchScreen extends React.Component {
     render() {
         return(
             <View style={{flex:1}}>
-                <View style={{flex:1}}>
-                    <TouchableNativeFeedback style={{flexDirection:'column',alignItems: 'center',justifyContent: 'center',padding:5}}>
-                        <View style={styles.searchBar}>
-                        <Image source={require('../search.png')} style={styles.ImageStyle} />
-                        <TextInput style={styles.input} underlineColorAndroid='rgba(0,0,0,0)' placeholder="Entrez votre ville ici..." />
-                        </View>
-                    </TouchableNativeFeedback >
+                <View style={{flex:1,flexDirection:'row',alignItems: 'center',justifyContent: 'center',padding:5}}>
+                    <View style={styles.searchBar}>
+                    <Image source={require('../search.png')} style={styles.ImageStyle} />
+                    <TextInput style={styles.input} underlineColorAndroid='rgba(0,0,0,0)' placeholder="Entrez votre ville ici..." />
+                    </View>
                 </View>
                 <View style={{flex:8}}>
+                {/* Ajouter ici la liste des résultats (limité à un chiffre choisi) */}
                 </View>
             </View>
 
@@ -43,7 +42,8 @@ const styles = StyleSheet.create({
       color: "#666666",
       fontWeight: 'bold',
       fontSize: 20,
-      padding: 10,
+      maxHeight:30,
+      margin: 10,
       flex:0.8,
     },
     ImageStyle: {
